861b2fc1a6b0409998a730f0a3200396e151d3b9
Leopold Cramer <2lcramer@gmail.com>
2019-11-07 10:13:40 +0100

    First Push

---
+++ Assets/Scripts/Tools/RotationInfluence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class RotationInfluence : MonoBehaviour {
+    
+	enum MovementType { Cran, Libre };
+
+    private void Start()
+    {
+    }
+
+    void PositionChange()
+    {
+
+    }
+
+    void RotationChange()
+    {
+
+    }
+
+    void ScaleChange()
+    {
+        
+    }
+}
