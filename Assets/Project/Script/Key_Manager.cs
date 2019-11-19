6dee1390f83576168121a17a2900c4dd87e64240
Jeff <jeffry648@hotmail.com>
2019-11-19 15:48:35 +0100

    Add : Key_Manager

---
+++ Assets/Project/Script/Key_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_Manager : MonoBehaviour {
+
+    [SerializeField] private GameObject _keyToInstantiate = null;
+    [SerializeField] private Transform _offset = null;
+    [SerializeField] private GameObject keyOfRoom;
+    
+    private void Start()
+    {
+        keyOfRoom = Instantiate(_keyToInstantiate, _offset.position, _offset.rotation);
+    }
+    void Update () {
+        if(keyOfRoom == null)
+        {
+
+        }
+		
+	}
+}
