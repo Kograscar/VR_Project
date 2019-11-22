e523f6839abf89ebbc79a28eba6a3ebfb2f394e9
Leopold Cramer <2lcramer@gmail.com>
2019-11-22 16:19:18 +0100

    Skrrrt Skrrrt Skrrrt

--- Assets/Project/Prog/Scripts/BuildSystem/CubeInstanciater.cs
+++ Assets/Project/Prog/Scripts/BuildSystem/CubeInstanciater.cs
@@ -142,7 +142,7 @@ public class CubeInstanciater : Singleton<CubeInstanciater> {
             if (_colorCount[_selectedColor] > 0)
             {
                 GameObject go = Instantiate(_selectedCube, _previewCube.transform.position, _previewCube.transform.rotation, _nearCube);
-                go.transform.localScale = new Vector3().normalized;
+                go.transform.localScale = new Vector3(1, 1, 1);
                 _placementTimer = 0;
                 _colorCount[_selectedColor]--;
             }
