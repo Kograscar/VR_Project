81331744a14c620f3be2d1817daf61796717fb23
Leopold Cramer <2lcramer@gmail.com>
2019-11-25 11:31:54 +0100

    Modify : ChuiLeCube + cube prefabs

--- Assets/Project/Prog/Scripts/BuildSystem/CubeInstanciater.cs
+++ Assets/Project/Prog/Scripts/BuildSystem/CubeInstanciater.cs
@@ -142,6 +142,7 @@ public class CubeInstanciater : Singleton<CubeInstanciater> {
             if (_colorCount[_selectedColor] > 0)
             {
                 GameObject go = Instantiate(_selectedCube, _previewCube.transform.position, _previewCube.transform.rotation, _nearCube);
+                Debug.Log("Place");
                 go.transform.localScale = new Vector3(1, 1, 1);
                 _placementTimer = 0;
                 _colorCount[_selectedColor]--;
