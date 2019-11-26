912d71d0c66e1bdfc4528780e684f94d724fe041
Leopold Cramer <2lcramer@gmail.com>
2019-11-26 10:39:27 +0100

    Modify : Gauntlet

---
+++ Assets/Project/Prog/Scripts/FollowRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRotation : MonoBehaviour {
+    
+    [SerializeField] Transform _objectToChange;
+    [SerializeField] Transform _objectToFollow;
+
+    private void Update()
+    {
+        _objectToChange.localRotation = _objectToFollow.localRotation;
+    }
+}
